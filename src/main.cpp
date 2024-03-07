3b9f9e2fbcb3184d53e9b1883cd2d36c6988a626
sergeykrasilnikov1 <s.krasilnikov2@g.nsu.ru>
2024-03-07 18:25:46 +1000

    2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "meta/meta.h"
 
 int main() {
-    double c = minus(sum(mul(5.2, 3.4),3.2), 3.0);
+    double c = minus(sum(mul(5.2, 3.4),3.2), 3.0)
     std::cout << c << std::endl;
     // Пример обращения:
     const unsigned f5 = factorial<5>::value; // 120
