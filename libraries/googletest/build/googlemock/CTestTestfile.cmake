5157ad7760c3761981a9b9026e2c7950d2d68dd7
sergeykrasilnikov1 <s.krasilnikov2@g.nsu.ru>
2024-03-07 18:07:20 +1000

    commit

---
+++ libraries/googletest/build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/c++/students/2 semestr/Seminar2/project-git/libraries/googletest/googlemock
+# Build directory: D:/c++/students/2 semestr/Seminar2/project-git/libraries/googletest/build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ libraries/googletest/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/c++/students/2 semestr/Seminar2/project-git/libraries/googletest/googletest
+# Build directory: D:/c++/students/2 semestr/Seminar2/project-git/libraries/googletest/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
